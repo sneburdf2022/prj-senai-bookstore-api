64bea44439cc0c79d7c1f7d25d8d265a9036286b
sneburdf2022 <39066466+sneburdf2022@users.noreply.github.com>
2023-02-15 20:28:38 -0300

    Ajustes

--- src/main/java/prj/senai/bookstore/resources/LivroResource.java
+++ src/main/java/prj/senai/bookstore/resources/LivroResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,17 +43,24 @@ public class LivroResource {
         return ResponseEntity.ok().body(listDTO);
     }
     @PostMapping
-    public ResponseEntity<Livro> create(@RequestBody Livro obj){
-        obj = service.create(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);
+    public ResponseEntity<Livro> create(
+        @RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
+        @RequestBody Livro obj){
+        Livro newObj = service.create(id_cat,obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
         //return ResponseEntity.created(uri).build();
     }
 
     @PutMapping(value="/{id}")
-    public ResponseEntity<LivroDTO> update(@PathVariable Integer id, @RequestBody LivroDTO obj){
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
         Livro newObj = service.update(id,obj);
-        return ResponseEntity.ok().body(new LivroDTO(newObj));
+        return ResponseEntity.ok().body(newObj);
+    }
+    @PatchMapping(value="/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+        Livro newObj = service.updatePatch(id,obj);
+        return ResponseEntity.ok().body(newObj);
     }
 
     @DeleteMapping(value="/{id}")
--- src/main/java/prj/senai/bookstore/service/LivroService.java
+++ src/main/java/prj/senai/bookstore/service/LivroService.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import prj.senai.bookstore.domain.Categoria;
 import prj.senai.bookstore.domain.Livro;
 import prj.senai.bookstore.dtos.LivroDTO;
 import prj.senai.bookstore.exceptions.ObjetNotFoundException;
@@ -30,9 +32,28 @@ public class LivroService {
         return repository.findAllByCategoria(id_cat);
     }
 
-    public Livro create(Livro obj) {
-        obj.setId(null);
-        return repository.save(obj);
+    public Livro updatePatch(Integer id, Livro obj) {
+        Livro newObj = findById(id);
+        updateDate(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    public Livro update(Integer id, Livro obj) {
+        Livro newObj = findById(id);
+        updateDate(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    private void updateDate(Livro newObj, Livro obj) {
+            if (obj.getTitulo() != null) {
+            newObj.setTitulo(obj.getTitulo());
+            }
+            if (obj.getNome_autor() != null) {
+                newObj.setNome_autor(obj.getNome_autor());
+        }
+            if (obj.getTexto() != null) {
+            newObj.setTexto(obj.getTexto());
+            }
     }
 
     public Livro update(Integer id, LivroDTO objDto) {
@@ -48,5 +69,12 @@ public class LivroService {
         repository.deleteById(id);
 
     }
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+        Categoria cat = categoriaServico.findById(id_cat);
+        obj.setCategoria(cat);
+        return repository.save(obj);
+	}
 
 }
