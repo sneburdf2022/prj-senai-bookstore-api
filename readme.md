2f9ff6173232f1141d588c4091c260ef7c8f4457
Rubens A da S L Oliveira <rubens_bsi@hotmail.com>
2023-08-06 11:26:11 -0300

    Ajustes para publicação

---
+++ readme.md
@@ -0,0 +1,20 @@
+<h1 align="center">Olá o Rubens Oliveira</h1>
+<h3 align="center">Esse projeto é um exmplo em java que uso como base para o pessoal treinar frontend</h3>
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=sneburdf2022&label=Profile%20views&color=0e75b6&style=flat" alt="sneburdf2022" /> </p>
+
+- 🤝 Utilizem como quiser
+<hr>
+
+- Esse **projeto** foi desenvolvido atravez de um treinamento de um profissional da área e **re-adaptado para fins de estudo**.
+- Liguagem utilizada: [Java String Boot](https://spring.io/projects/spring-boot) <img src="https://www.vectorlogo.zone/logos/springio/springio-icon.svg" alt="spring" width="16" height="16"/>
+<hr>
+### Para rempacotar e executar:
+
+
+
+```prompt
+mvn clean package spring-boot:repackage
+```
+```power-shell
+java -jar ProjetoApplication.war
+```
--- src/main/java/prj/senai/bookstore/resources/LivroResource.java
+++ src/main/java/prj/senai/bookstore/resources/LivroResource.java
@@ -21,53 +21,56 @@ import prj.senai.bookstore.service.LivroService;
 import java.util.List;
 import jakarta.validation.Valid;
 import java.util.stream.Collectors;
-import java.net.URI; 
+import java.net.URI;
 import prj.senai.bookstore.dtos.LivroDTO;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping(value="/livros")
+@RequestMapping(value = "/livros")
 public class LivroResource {
-    
+
     @Autowired
     private LivroService service;
 
-    @GetMapping(value="/{id}" )
-    public ResponseEntity<Livro> findById(@PathVariable Integer id){
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Livro> findById(@PathVariable Integer id) {
         Livro obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
     @GetMapping
-    public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+    public ResponseEntity<List<LivroDTO>> findAll(
+            @RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
         List<Livro> list = service.findAll(id_cat);
         List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
-    
-    @PutMapping(value="/{id}")
-    public ResponseEntity<Livro> update(@PathVariable Integer id, @Valid @RequestBody Livro obj){
-        Livro newObj = service.update(id,obj);
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @Valid @RequestBody Livro obj) {
+        Livro newObj = service.update(id, obj);
         return ResponseEntity.ok().body(newObj);
     }
-    @PatchMapping(value="/{id}")
-    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id,@Valid  @RequestBody Livro obj){
-        Livro newObj = service.updatePatch(id,obj);
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @Valid @RequestBody Livro obj) {
+        Livro newObj = service.updatePatch(id, obj);
         return ResponseEntity.ok().body(newObj);
     }
 
     @PostMapping
     public ResponseEntity<Livro> create(
-        @RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
-        @Valid @RequestBody Livro obj){
-        Livro newObj = service.create(id_cat,obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+            @RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
+            @Valid @RequestBody Livro obj) {
+        Livro newObj = service.create(id_cat, obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}")
+                .buildAndExpand(newObj.getId()).toUri();
         return ResponseEntity.created(uri).build();
-        //return ResponseEntity.created(uri).build();
+        // return ResponseEntity.created(uri).build();
     }
 
-    @DeleteMapping(value="/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Integer id){
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
 
